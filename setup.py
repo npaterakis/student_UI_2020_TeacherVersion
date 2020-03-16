f4fb1a563a9a837f26f1bb36bba54bb29eea5455
npaterakis <45340739+npaterakis@users.noreply.github.com>
2020-03-16 18:20:38 +0100

    First version

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='student_UI_2020',
+    version='',
+    packages=['scripts', 'scripts.forecastingUtils'],
+    url='',
+    license='',
+    author='npaterakis',
+    author_email='',
+    description=''
+)
